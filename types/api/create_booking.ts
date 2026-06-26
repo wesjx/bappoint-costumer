834e8c1b9ff51b403fec89d369c741f8f7225d61
Wesley Junio <wesjrpiri@gmail.com>
2026-06-26 19:21:52 +0100

    feat: create interface to get create booking data.

---
+++ types/api/create_booking.ts
@@ -0,0 +1,17 @@
+import { AppointmentStatus } from "@/enum/appointment_status"
+
+export interface CreateBooking{
+    createdAt: string
+    paymentDeadLine: string
+    costumerName: string
+    costumerEmail: string
+    costumerPhone: string
+    appointmentDate: string
+    startTime: string
+    endTime: string
+    totalAmount: number
+    appointmentStatus: AppointmentStatus
+    stripeSessionId: string
+    serviceIds: string
+    companyId: string
+}
