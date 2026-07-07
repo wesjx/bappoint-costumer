e8232149825c649f399a9bb0269938ee84f57cf0
Wesley Junio <wesjrpiri@gmail.com>
2026-07-07 17:59:33 +0100

    fix: update types of company dto.

--- types/api/company.ts
+++ types/api/company.ts
@@ -1,17 +1,24 @@
 import { AppointmentInterval } from "@/enum/appointment_interval"
+import { OffDayDTO } from "./off_days"
+import { OperatingHoursDTO } from "./operating_hours"
+import { ServiceDTO } from "./services"
 
 export interface CompanySettingsDTO {
-    id: string
-    appointmentInterval: AppointmentInterval
-    maxCancellationInterval: number
-  }
-  
-  export interface CompanyDTO {
-    id: string
-    name: string
-    email: string
-    phone: string
-    address: string
-    stripeAccountId: string
-    settings: CompanySettingsDTO
-  }
+  id: string
+  appointmentInterval: AppointmentInterval
+  maxCancellationInterval: number
+  services: ServiceDTO[]
+  operatingHours: OperatingHoursDTO[]
+  offDays: OffDayDTO[]
+}
+
+export interface CompanyDTO {
+  id: string
+  name: string
+  email: string
+  phone: string
+  address: string
+  stripeAccountId: string
+  depositPercentage: number
+  settings: CompanySettingsDTO
+}
