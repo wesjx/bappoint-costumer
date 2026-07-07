bc141b9996272b65d206e17c4fc0d3ef3dcc6fe3
Wesley Junio <wesjrpiri@gmail.com>
2026-07-07 18:00:49 +0100

    feat: update validation for booking form.

--- types/booking-form.ts
+++ types/booking-form.ts
@@ -1,16 +1,14 @@
-import { Path, UseFormReturn } from "react-hook-form";
-import z from "zod";
+import { Path, UseFormReturn } from "react-hook-form"
+import z from "zod"
 
 export const formSchema = z.object({
-    name: z.string(),
-    booking: z.date(),
-    time: z.string().min(1, "Select one time."),
-    services: z.array(z.number().int().min(1)),
+  name: z.string(),
+  booking: z.date(),
+  time: z.string().min(1, "Select one time."),
+  services: z.array(z.string().min(1)),
 })
 
 export type BookingFormType = z.infer<typeof formSchema>
-
 export type BookingForm = UseFormReturn<BookingFormType>
-
 export type FormFieldName = Path<BookingFormType>
 export type FormFieldServices = Path<BookingFormType>
